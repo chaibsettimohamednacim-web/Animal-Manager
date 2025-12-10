b86a2b554d1a1489e4228ff2c75d3ed2332d0bbf
Mohamed Chaib Setti <chaibse231@CAMPUS@V304V-NCP049.campus.unicaen.fr>
2025-12-10 17:40:58 +0100

    le respect des bonnes pratiques de sécurité

--- src/control/Controller.php
+++ src/control/Controller.php
@@ -34,34 +34,53 @@ class Controller{
         $this->view->prepareAnimalCreationPage($af);
     }
 
-   public function saveNewAnimal(array $data){
+    public function saveNewAnimal(array $data){
+
+        $allowedImageTypes = [
+            IMAGETYPE_GIF, 
+            IMAGETYPE_JPEG, 
+            IMAGETYPE_PNG
+        ];
+        
         $movePath = null;
-        if (isset($_FILES[AnimalBuilder::IMAGE_REF]) && $_FILES[AnimalBuilder::IMAGE_REF]['error'] !== UPLOAD_ERR_NO_FILE) {
-            $imageTmpPath = $_FILES[AnimalBuilder::IMAGE_REF]['tmp_name'];
-            $imageName = basename($_FILES[AnimalBuilder::IMAGE_REF]['name']);
-            $fileExtension = pathinfo($imageName, PATHINFO_EXTENSION);
-            $uniqueName = hash('sha256', time() . $imageName) . '.' . $fileExtension;
-            $rootDir = dirname(__DIR__, 2);
-            $movePath = $rootDir . '/images/' . $uniqueName;
-            if ($_FILES[AnimalBuilder::IMAGE_REF]['error'] === UPLOAD_ERR_OK && move_uploaded_file($imageTmpPath, $movePath)) {
-                $data[AnimalBuilder::IMAGE_REF] = 'images/' . $uniqueName;
-            } else if ($_FILES[AnimalBuilder::IMAGE_REF]['error'] !== UPLOAD_ERR_OK) {
-                 $data[AnimalBuilder::IMAGE_REF] = null;
+        $data[AnimalBuilder::IMAGE_REF] = null; 
+        
+        if (isset($_FILES[AnimalBuilder::IMAGE_REF])) {
+            $fileInfo = $_FILES[AnimalBuilder::IMAGE_REF];
+            if ($fileInfo['error'] === UPLOAD_ERR_OK) {
+                
+                $imageTmpPath = $fileInfo['tmp_name'];
+                $imageType = exif_imagetype($imageTmpPath);
+                
+                if ($imageType !== false && in_array($imageType, $allowedImageTypes)) {
+                    $fileExtension = image_type_to_extension($imageType, false);
+                    $uniqueName = hash('sha256', time() . $fileInfo['name']) . '.' . $fileExtension;
+                    $rootDir = dirname(__DIR__, 2); 
+                    $movePath = $rootDir . '/images/' . $uniqueName; 
+                    if (move_uploaded_file($imageTmpPath, $movePath)) {
+                        $data[AnimalBuilder::IMAGE_REF] = 'images/' . $uniqueName;
+                    }
+                }
+            } else if ($fileInfo['error'] !== UPLOAD_ERR_NO_FILE) {
+                $data[AnimalBuilder::IMAGE_REF] = null;
             }
         } 
+        
         $af = new AnimalBuilder($data);
+        
         if($af->isValid()){
             $animal = $af->createAnimal();
             $id = $this->animalStorage->create($animal);
             $this->view->displayAnimalCreationSuccess($id);
         }
         else{
-            if ($movePath !== null && $data[AnimalBuilder::IMAGE_REF] === null && file_exists($movePath)) {
+            if ($movePath !== null && file_exists($movePath)) {
                 unlink($movePath); 
             }
             $this->view->prepareAnimalCreationPage($af);
         }
     }
+
 }
 
 
